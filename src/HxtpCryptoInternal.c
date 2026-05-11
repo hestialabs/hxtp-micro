6c9b72dc7cf55289d27d75d53107d6ec3549bfd1
dedsec <236255118+hestialabs@users.noreply.github.com>
2026-05-11 17:25:43 +0530

    refactor: move variable declaration to inner loop and add explicit cast in pack25519

--- src/HxtpCryptoInternal.c
+++ src/HxtpCryptoInternal.c
@@ -69,20 +69,21 @@ static void sel25519(gf p,gf q,int b) {
 }
 
 static void pack25519(u8 *o,const gf n) {
-    int i,j,b;
+    int i,j;
     gf m,t;
     FOR(i,16) t[i]=n[i];
     car25519(t);
     car25519(t);
     car25519(t);
     FOR(j,2) {
+        int b;
         m[0]=t[0]-0xffed;
         for(i=1;i<15;i++) {
             m[i]=t[i]-0xffff-((m[i-1]>>16)&1);
             m[i-1]&=0xffff;
         }
         m[15]=t[15]-0x7fff-((m[14]>>16)&1);
-        b=(m[15]>>16)&1;
+        b=(int)((m[15]>>16)&1);
         m[14]&=0xffff;
         sel25519(t,m,1-b);
     }
